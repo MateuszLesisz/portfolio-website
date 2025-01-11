775e1e4b02cb883dcbadde29df0ee796e352d02b
Mateusz Lesisz <mateusz.lesisz@tss.black>
2025-01-11 20:37:09 +0100

    add some model updates

--- components/Model.js
+++ components/Model.js
@@ -10,11 +10,13 @@ const Model = ({state, details, toggleModel}) => {
             <div className="max-w-screen-xl w-full bg-white dark:bg-zinc-900 p-6 rounded-lg border border-zinc-100
             dark:border-zinc-700 max-h-[80vh]">
                 <div className="flex flex-wrap lg:space-x-6">
-                    <div className="relative w-full lg:w-[400px] pt-[56.25%] rounded-lg overflow-hidden">
-                        <Image src={details.image}
-                               fill
-                               className="w-full h-full absolute inset-0"
-                               alt=""/>
+                    <div className="relative w-full lg:w-[400px] rounded-lg overflow-hidden">
+                        <div className="pt-[56.25%]">
+                            <Image src={details.image}
+                                   fill
+                                   className="w-full h-full absolute inset-0"
+                                   alt=""/>
+                        </div>
                     </div>
                 </div>
             </div>
