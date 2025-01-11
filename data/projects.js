6ca280775d9c5f0e48a2116670d416bf764155d8
Mateusz Lesisz <mateusz.lesisz@tss.black>
2025-01-11 21:33:49 +0100

    change word

--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,7 @@ const data = [
         title: "Portfolio Website using NextJS 14 and Tailwind CSS",
         image: "/projects/portfolio.png",
         description: "This is the project of the website you are currently viewing." +
-            " Inspired by the fact that not everything can be described in a CV, here I publish all the information about the projects I have worked on – both professional and personal." +
+            " Inspired by the fact that not everything can be described in a CV. Here I publish all the information about the projects I have worked on – both professional and personal." +
             "The website is fully responsive and supports various types of devices, allowing you to comfortably explore my 'extended CV' on a desktop computer, phone, or tablet" +
             " The website was built using NodeJS 14 and Tailwind CSS.",
         skills: [
