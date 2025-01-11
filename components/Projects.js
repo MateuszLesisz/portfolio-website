b0ee4cc23ae9a21f49cdfebf6d70c0fd1badc718
Mateusz Lesisz <mateusz.lesisz@tss.black>
2025-01-11 15:38:39 +0100

    fix aspect ratio

--- components/Projects.js
+++ components/Projects.js
@@ -9,11 +9,11 @@ const Projects = () => {
             <Heading>projects</Heading>
             {data.map((project) => (
                 <div key={project.id} className="flex flex-grap gap-5 w-full lg:w=8/12 mb=10">
-                    <div className="relative w-full md:w-[120px] h=[220px] md:h-20 rounded-lg overflow-hidden">
-                        <Image src={project.image} fill className="w-full h-full" alt="project image"/>
+                    <div className="relative w-full md:w-[120px] pt-[56.25%] md:pt-0 md:h-20 rounded-lg overflow-hidden">
+                        <Image src={project.image} fill className="w-full h-full absolute top-0 lef-0 bottom-0 right-0" alt="project image"/>
                     </div>
                     <div className="flex-1">
-                        <h2 className="text-x1 font-bold text-zinc-700 dark:text-zinc-400 leading-[25px]">
+                        <h2 className="text-lg md:text-x1 font-bold text-zinc-700 dark:text-zinc-400 leading-[25px]">
                             {project.title}
                         </h2>
                         <p className="text-base font-semibold text-zinc-500 dark:text-zinc-400 mt-2"
