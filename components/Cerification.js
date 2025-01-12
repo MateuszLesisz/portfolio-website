dba54775f79c023fb7bd7a21ce1a4fa1f192522f
Mateusz Lesisz <m.lesisz97@wp.pl>
2025-01-12 18:27:15 +0100

    delete comments

--- components/Cerification.js
+++ components/Cerification.js
@@ -1,15 +1,15 @@
 'use client';
 import SectionWrapper from "@/components/SectionWrapper";
 import Heading from "@/components/Heading";
-import data from "@/data/certification"; // Import pliku danych
+import data from "@/data/certification";
 import Image from "next/image";
 import { FaArrowTrendUp } from "react-icons/fa6";
 import { useState } from "react";
-import CertViewerModel from "@/components/CertViewerModel"; // Import komponentu do podglądu PDF
+import CertViewerModel from "@/components/CertViewerModel";
 
 const Certification = () => {
-    const [state, setState] = useState(false); // Kontroluje widoczność modala
-    const [filePath, setFilePath] = useState(""); // Przechowuje ścieżkę do PDF
+    const [state, setState] = useState(false);
+    const [filePath, setFilePath] = useState("");
 
     const toggleModel = () => {
         setState(!state);
@@ -21,27 +21,25 @@ const Certification = () => {
                 <Heading>Certifications</Heading>
                 {data.map((certification) => (
                     <div key={certification.id} className="flex gap-5 w-full lg:w-8/12 mb-10">
-                        {/* Obraz certyfikatu */}
                         <div className="relative w-[150px] h-[150px] rounded-lg overflow-hidden">
                             <Image
-                                src={certification.image} // Obraz pobierany z danych
+                                src={certification.image}
                                 width={150}
                                 height={150}
                                 className="object-contain"
                                 alt={`${certification.title} image`}
                             />
                         </div>
-                        {/* Nazwa certyfikatu i strzałka */}
                         <div className="flex items-center">
                             <div className="flex space-x-3 items-center">
                                 <h2 className="text-lg font-bold text-zinc-700 dark:text-zinc-400">
-                                    {certification.title} {/* Tytuł pobierany z danych */}
+                                    {certification.title}
                                 </h2>
                                 <FaArrowTrendUp
                                     className="text-[20px] test-zinc-900 dark:text-white cursor-pointer"
                                     onClick={() => {
-                                        setFilePath(certification.filePath); // Ustawienie ścieżki do PDF
-                                        toggleModel(); // Otwórz modal
+                                        setFilePath(certification.filePath);
+                                        toggleModel();
                                     }}
                                 />
                             </div>
@@ -49,8 +47,6 @@ const Certification = () => {
                     </div>
                 ))}
             </SectionWrapper>
-
-            {/* Modal do podglądu PDF */}
             <CertViewerModel state={state} filePath={filePath} toggleModal={toggleModel} />
         </section>
     );
--- components/CertViewerModel.js
+++ components/CertViewerModel.js
@@ -2,27 +2,25 @@ import { useState } from "react";
 import { MdClose } from "react-icons/md";
 
 const CertViewerModel = ({ state, filePath, toggleModal }) => {
-    const [dimensions, setDimensions] = useState({ width: 0, height: 0 }); // Wymiary obrazu
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
     const handleImageLoad = (e) => {
         const { naturalWidth, naturalHeight } = e.target;
-        setDimensions({ width: naturalWidth, height: naturalHeight }); // Ustawiamy rzeczywiste wymiary obrazu
+        setDimensions({ width: naturalWidth, height: naturalHeight });
     };
 
     return state ? (
         <div
             className="fixed inset-0 bg-black-25 p-4 backdrop-blur z-[99999999999] flex items-center justify-center"
         >
-            {/* Przycisk zamykania */}
             <MdClose
                 onClick={toggleModal}
                 className="cursor-pointer text-[25px] absolute top-6 right-6 text-black dark:text-white"
             />
-            {/* Kontener obrazu */}
             <div
                 className="bg-white dark:bg-zinc-900 p-4 rounded-lg border border-zinc-100 dark:border-zinc-700"
                 style={{
-                    width: `${dimensions.width || 500}px`, // Domyślnie 500px, jeśli wymiary nie są ustawione
+                    width: `${dimensions.width || 500}px`,
                     height: `${dimensions.height || 500}px`,
                 }}
             >
@@ -30,7 +28,7 @@ const CertViewerModel = ({ state, filePath, toggleModal }) => {
                     src={filePath}
                     alt="Certificate Preview"
                     className="w-full h-full object-contain rounded-lg"
-                    onLoad={handleImageLoad} // Wywołanie funkcji po załadowaniu obrazu
+                    onLoad={handleImageLoad}
                 />
             </div>
         </div>
